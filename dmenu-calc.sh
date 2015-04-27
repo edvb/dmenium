9b9714b024798f0963aef715e9e463c7ac4b96fa
Ed van Bruggen <edvb54@gmail.com>
2015-04-26 20:43:18 -0700

    Add more comments to dmenu-calc.sh

--- dmenu-calc.sh
+++ dmenu-calc.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-# Caluctor using dmenu as interface
+# Calculator using dmenu as interface
 
 # run dmenu calculator while the result is not empty
 while : ; do
+    # use contents of clipboard as option, pass into bc to be calculated
     result=$(xclip -o -sel clip | dmenu -p Calc | xargs echo | bc 2>&1)
-    if [[ $result ]]; then # if result is not empty (the last one), copy to clipboard
+    if [[ $result ]]; then # if result is empty (the last one), don't copy it to clipboard
         echo $result | xclip -sel clip
     fi
     [[ $result ]] || break # if result is empty, break from while loop
