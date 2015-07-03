bad221e45d58fa305881d8595e1816e94266c887
Ed van Bruggen <edvb54@gmail.com>
2015-07-02 21:43:28 -0700

    Replace xclip with xsel

--- dmenu-calc.sh
+++ dmenu-calc.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-# Calculator using dmenu as interface
+# Calculator using dmenu as an interface
 
 # run dmenu calculator while the result is not empty
 while : ; do
     # use contents of clipboard as option, pass into bc to be calculated
-    result=$(xclip -o -sel clip | dmenu -p Calc | xargs echo | bc 2>&1)
+    result=$(xsel -o -b | dmenu -p Calc | xargs echo | bc 2>&1)
     if [[ $result ]]; then # if result is empty (the last one), don't copy it to clipboard
-        echo $result | xclip -sel clip
+        printf "$result" | xsel -b
     fi
     [[ $result ]] || break # if result is empty, break from while loop
 done
