f75f613f1d674f4257f970f6d40eb83f40163f00
Ed van Bruggen <edvb54@gmail.com>
2016-11-02 19:52:43 -0700

    Change win.sh to use lsw to list open windows

--- win.sh
+++ win.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Use dmenu to focus on an open window
 
-win="$(wmctrl -l | dmenu -l 10 "$@" | cut -d ' ' -f 1)"
+win="$(lsw | dmenu -i -l 10 "$@" | cut -d ' ' -f 1)"
 [ "$win" ] && wmctrl -ia "$win" &
